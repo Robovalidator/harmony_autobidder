f86e0e635ecea657b4b39c4aaf44c439de3b0580
hsiung <hsiung@pinterest.com>
2020-05-27 21:10:42 -0700

    cleanup

--- logic/bidding_logic.py
+++ logic/bidding_logic.py
@@ -6,22 +6,20 @@ from logic import validator_logic
 def run_autobid(prev_slot_range=None):
     validators = validator_logic.get_all_validators()
     my_slot_range, my_validator = validator_logic.get_my_slot_range_for_validators(validators)
-    response_json = {"action": None}
-
-    changed = prev_slot_range != my_slot_range
-    if changed:
-        response_json["slots"] = str(my_slot_range)
-        response_json["uptime"] = str(my_validator.uptime_as_pct)
-        response_json["validator"] = my_validator.to_dict()
-        response_json["validators"] = [v.to_dict() for v in validators]
+    response_json = dict(
+        action=None,
+        slots=str(my_slot_range),
+        uptime=str(my_validator.uptime_as_pct),
+        validator=my_validator.to_dict(),
+        validators=[v.to_dict() for v in validators]
+    )
 
     validator_lower_bid, key_to_add = validator_logic.get_validator_add_key(my_validator)
     if validator_lower_bid:
         validators_lowering_bid = [validator_lower_bid if v.address == config.VALIDATOR_ADDR else v for v in validators]
         next_slot_range, next_validator = validator_logic.get_my_slot_range_for_validators(validators_lowering_bid)
-        if changed:
-            slot_str = str(next_slot_range)
-            response_json["slots_after_lowering_bid"] = slot_str
+        response_json["slots_after_lowering_bid"] = str(next_slot_range)
+
         if my_slot_range.end <= config.MIN_SLOT and next_slot_range.end <= config.MAX_SLOT:
             response_json["action"] = u"Lowering the bid by adding key {}".format(key_to_add)
             response_json["added_bls_key"] = key_to_add
@@ -35,9 +33,7 @@ def run_autobid(prev_slot_range=None):
         validators_increasing_bid = [validator_increase_bid if v.address == config.VALIDATOR_ADDR else v
                                      for v in validators]
         next_slot_range, next_validator = validator_logic.get_my_slot_range_for_validators(validators_increasing_bid)
-        if changed:
-            slot_str = str(next_slot_range)
-            response_json["slots_after_increasing_bid"] = slot_str
+        response_json["slots_after_increasing_bid"] = str(next_slot_range)
         if my_slot_range.end >= config.MAX_SLOT:
             response_json["action"] = u"Increasing the bid by removing key {}".format(key_to_remove)
             client.remove_bls_key(key_to_remove)

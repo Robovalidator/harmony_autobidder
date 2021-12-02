66329eced42963431f5458e32f5fd282f33cf11d
hsiung <hsiung@pinterest.com>
2021-12-02 07:52:33 -0600

    Separate out bls key strings to avoid merge conflicts

---
+++ bls_keys.py
@@ -0,0 +1,8 @@
+# Replace the strings below with your own BLS key strings in order of priority across ALL nodes
+
+BLS_KEYS = (
+    "f82d30adadabaaaeba00406a5d607134343888dccf4fc45bdc22f02ad10df3ddeed1656a2a253262dae92095297e3f84",
+    "8be0121e5282071287886b07af678eefb67737f12d306c370f588d2612bf3020f9dc97fce63ee86c0ee3cd2153e43f90",
+    "2bcee590cb191a7bff14641fb0afdd6e07ee83b45e7247fce9dcf0fc10d8c7c3560dad54891cdf8bffff36e4a2c24f04",
+    "1c1d7ca4562ddda2cb456a766f86f487ff30bf2d3cbb692f359a29438b04cb6b79daf9241c44b514893076a14bea9984",
+)
